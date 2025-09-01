1ec79df7a037e6b9efac833191bafbf2c9a8a246
Aklil Zenebe Kiflay <56843738+azkiflay@users.noreply.github.com>
2025-09-01 10:17:56 +0100

    Create app.py

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+# tests/test_app.py
+import json
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    data = json.loads(response.data)
+    assert response.status_code == 200
+    assert data["message"] == "Hello from GitHub Actions CI/CD with Python!"
+
+def test_addition():
+    client = app.test_client()
+    response = client.get("/add/3/4")
+    data = json.loads(response.data)
+    assert response.status_code == 200
+    assert data["result"] == 7
